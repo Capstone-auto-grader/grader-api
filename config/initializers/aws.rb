6b05b4a397eca9626350e9972508755a45a68c18
sternj <binaryman00010@gmail.com>
2018-12-25 19:38:45 -0500

    added shit

---
+++ config/initializers/aws.rb
@@ -0,0 +1,10 @@
+require 'aws-sdk'
+
+Aws.config.update({
+    region: 'us-east-1',
+    access_key_id: ENV['ACCESS_KEY'],
+    secret_access_key: ENV['SECRET_KEY'],
+})
+
+
+S3_BUCKET = Aws::S3::Resource.new.bucket('auto-grader-capstone')
