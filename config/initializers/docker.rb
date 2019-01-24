ad20fd92aff1d643ac4cff852d5d7ab6cf14f13a
sternj <binaryman00010@gmail.com>
2019-01-24 09:53:44 -0500

    did aaa

--- config/initializers/docker.rb
+++ config/initializers/docker.rb
@@ -1,4 +1,4 @@
 require 'docker'
 Docker.url = ENV['DOCKER_URL']
-Excon.defaults[:write_timeout] = 1000
-Excon.defaults[:read_timeout] = 1000
+# Excon.defaults[:write_timeout] = 1000
+# Excon.defaults[:read_timeout] = 1000
