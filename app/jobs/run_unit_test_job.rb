39a180c4aac9665c3c9c8fd27eacb39a0270b3d3
sternj <binaryman00010@gmail.com>
2019-01-28 21:28:28 -0500

    Finalized

--- app/jobs/run_unit_test_job.rb
+++ app/jobs/run_unit_test_job.rb
@@ -20,7 +20,7 @@ class RunUnitTestJob < ApplicationJob
     img = Container.find_by(name: image_name)
     # puts img
     container = Docker::Container.create('Image' => img.uid,
-                                         'Env' => ["AWS_SECRET_ACCESS_KEY=#{SECRET_KEY}", "AWS_ACCESS_KEY_ID=#{ACCESS_KEY}",'_JAVA_OPTIONS="-XX:MaxPermSize=512m -Xmx512m"'],
+                                         'Env' => ["AWS_SECRET_ACCESS_KEY=#{SECRET_KEY}", "AWS_ACCESS_KEY_ID=#{ACCESS_KEY}"],
                                          'Cmd' => ['./unzip-and-grade.sh', project_uri, test_uri, student_name],
                                          'Tty' => true)
     submission.update_attribute(:container_id, container.id)
